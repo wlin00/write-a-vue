1bbdc3ff5cae60e5fe3f3f41595b8e16d0dbecd7
wlin00 <wlin0z@163.com>
2022-07-15 20:39:59 +0800

    add:vue2初始化- 对数组类型的数据特殊处理，自定义数组原型重写7个方法实现数据挟持

---
+++ src/observe/array.js
@@ -0,0 +1,40 @@
+const oldArrayProto = Array.prototype // 获取数组原型
+
+export let newArrayProto = Object.create(oldArrayProto) // 自定义数组实例，继承于普通数组 newArrayProto.__proto__ = Array.prototype
+
+// 重写7个可能改变当前数组本身的操作，其中监听push、unshift、splice这三个可能新增数组元素的方法，若出现新增则对新增的元素做数据挟持
+const methods = [
+  'push',
+  'pop',
+  'shift',
+  'unshift',
+  'splice',
+  'sort',
+  'reverse',
+]
+
+methods.forEach((method) => {
+  newArrayProto[method] = function (...args) {
+    console.log('run custom method')
+    let result = oldArrayProto[method].call(this, ...args) // 先获取原数组操作的结果
+    const ob = this.__ob__ // 获取 observe 中对当前实例this进行的标记
+    // 监听数组的新增操作, 对新增的数组元素添加数据挟持（push、unshift、splice）
+    let inserted
+    switch (method) {
+      case 'push':
+      case 'unshift':
+        inserted = args
+        break;
+      case 'splice':
+        inserted = args.slice(2) // 获取splice的第三个以后的参数
+        break;
+      default:
+        break;
+    }
+    if (inserted) {
+      ob.observeArray(inserted) // 若本次操作有新增数组元素，对新增的数组元素做数据挟持    
+    }
+    console.log('新增数组数据', inserted)
+    return result // 返回原数组操作的结果
+  }    
+})
--- src/observe/index.js
+++ src/observe/index.js
@@ -1,8 +1,32 @@
+import { newArrayProto } from './array'
 class Observer {
   constructor(data) {
-    this.walk(data)
+    /**
+     *  1、object.defineProperty 只能挟持当前存在的属性，对新增的、删除的属性无法进行数据挟持
+     *  因此在Vue2中需要写一些单独的api 如$set、 $delete
+     *  2、object.defineProperty 只能挟持对象内的普通数据类型，若是内部的对象需要递归数据挟持
+     *  3、object.defineProperty 处理数组元素时，会有较高性能损耗，所以vue2里是对数组是进行特殊处理
+    */
+
+    Object.defineProperty(data, '__ob__', { // 每次observe数据挟持，对当前实例添加属性标记，后续如果一个实例已经创建或者标记过就直接返回
+      enumerable: false,
+      value: this
+    })
+
+    // 为了避免使用Object.defineProperty对数组进行挟持的性能损耗，特殊对数组进行处理
+    if (Array.isArray(data)) {
+      data.__proto__ = newArrayProto // 当前实例继承于自定义数组原型
+      this.observeArray(data)
+    } else { // 处理对象
+      this.walk(data)
+    }
   }
-  walk(data) { // 对data内部每个属性做递归的数据挟持
+
+  observeArray(arr) { // 对数组进行挟持，遍历数组递归数据挟持
+    arr.forEach((item) => observe(item))
+  }
+  
+  walk(data) { // 对data内部每个属性做递归的数据挟持, 此处会重新定义属性，相当于data中的数据复制一遍
     Object.keys(data).forEach((key) => defineReactive(data, key, data[key]))
   }
 }
@@ -29,6 +53,9 @@ function defineReactive(target, key, value) {
 export function observe(data) {
   if (typeof data !== 'object' || data === null) { // 若当前传入非对象则 或 为null则return
     return
+  }
+  if (data.__ob__ instanceof Observer) { // 若当前实例已经创建和标记过，就直接返回；避免死循环
+    return data.__ob__
   }
   return new Observer(data) // 是对象就对这个对象进行数据挟持，这主要是可以用于对象内部的属性做递归的挟持
 }
